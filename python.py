d654ac8d6d6264bdac878aacfe191e5f4ce0c029
Xz1998 <17852059500@163.com>
2019-06-18 01:09:27 +0800

    python.py

---
+++ python.py
@@ -0,0 +1,35 @@
+# python version: 3.7.2	
+# author: bavdu
+# email: bavduer@163.com
+# date: 2019/06/14
+# usage: closure
+def logAnalysis(path):
+    def accessLog(frequency):
+        if type(frequency) != dict or len(frequency) != 0:
+            prompt = 'Please Check variable must be list type and len equal zero.'
+            return prompt
+        with open(path, 'r+', encoding='utf8') as logfile:
+            for line in logfile.readlines():
+                if line.split()[0] not in frequency:
+                    frequency[line.split()[0]] = 1
+                else:
+                    frequency[line.split()[0]] += 1
+        return frequency
+    return accessLog
+
+
+log = logAnalysis('./access_log-20181111')
+analysis = log({})
+
+ipList = [element for element in analysis.items()]
+for i in range(len(ipList)):
+    for j in range(i, len(ipList)):	
+        if ipList[i][1] < ipList[j][1]:
+            ipList[i], ipList[j] = ipList[j], ipList[i]
+print(ipList[0:10])
+
+[root@git Cloud1903LearnGit]# git add --all
+[root@git Cloud1903LearnGit]# git commit -m 'log analysis program.'
+[master 6f4fdea] log analysis program.
+ 2 files changed, 50 insertions(+)
+ create mode 100644 ProjectApacheLog.py
